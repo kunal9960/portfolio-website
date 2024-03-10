7984332d6bbf590f6c5f37fc9985bd2c42f70aec
Kunal <kunald9960@gmail.com>
2024-03-10 20:48:38 +0530

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/image.png")
+
+with col2:
+    st.title("Kunal Dalvi")
+    content = """
+    Hi, I am Kunal! I am a Python programmer and a passionate data analysis enthusiast, 
+    dedicated to unraveling insights from complex datasets through statistical methods and programming.
+    """
+    st.info(content)
