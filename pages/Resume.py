fa5a84fac4d8fc5498da46c37b1b05fe407f11d9
Kunal Dalvi <kunald9960@gmail.com>
2024-03-27 23:02:30 +0530

    Add Resume page

--- pages/Resume.py
+++ pages/Resume.py
@@ -1,14 +1,26 @@
 import streamlit as st
-from send_email import send_email
+import base64
 
-st.set_page_config(layout="wide")
-st.markdown(
-    """
-    <style>
-        section[data-testid="stSidebar"] {
-            width: 100px !important;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+
+def displaypdf(file_path):
+    # Opening file from file path
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+    # Embedding PDF in HTML
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000" type="application/pdf"></iframe>'
+
+    # Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+
+def main():
+    st.title("Resume")
+    st.write("Below is my resume:")
+
+    # Call displaypdf function with the path to your PDF file
+    displaypdf("pages/Resume.pdf")
+
+
+if __name__ == "__main__":
+    main()
